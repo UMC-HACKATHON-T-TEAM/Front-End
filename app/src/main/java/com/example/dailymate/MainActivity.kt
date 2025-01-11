d55c1a61ae090fdf01a991615950b80196a3fd76
장수민 <20221416@sungshin.ac.kr>
2025-01-11 20:59:47 +0900

    init commit

--- app/src/main/java/com/example/dailymate/MainActivity.kt
+++ app/src/main/java/com/example/dailymate/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.dailymate
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.dailymate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // ViewBinding 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.apply {
+            itemRippleColor = null // Ripple 효과 제거
+            setBackgroundColor(Color.TRANSPARENT) // 배경 투명 설정
         }
+
     }
 }
